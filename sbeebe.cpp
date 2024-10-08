b743d29f9ea9cf8b4bc9bd40eeb3fff1a024d579
Skye Beebe <sbeebe@odin.cs.csubak.edu>
2024-10-08 09:46:31 -0700

    Updated my file to create timer

--- sbeebe.cpp
+++ sbeebe.cpp
@@ -1,28 +1,10 @@
-#include <iostream>
+#include <fonts.h>
+#include<chrono>
 
 using namespace std;
 
-class Global {
-    public:
+class Timer {
+    int d1 = typedef duration<signed int-type >= 35 bits> 
 
-}g;
-
-if (e->type == MotionNotify) {
-    //The mouse moved!
-   /* if (savex != e->xbutton.x || savey != e->xbutton.y) {
-        savex = e->xbutton.x;
-        savey = e->xbutton.y;
-        int y = g.yres - e->xbutton.y;*/
-        //Code placed here will execute whenever the mouse moves.
-        for (int i = 0; i < 2; i++) {
-            if (n < MAX_PARTICLES) {
-                particle[n].pos[0] = x;
-                particle[n].pos[1] = y;
-                particle[n].w = 4;
-                particle[n].h = 4;
-                particle[n].vel[0] = rnd() * 1.0 - 0.5;
-                particle[n].vel[1] = rnd() * 1.0 - 0.5;
-                ++n;
-            }
-
-images
+    ggprint8b(&r, 16, c, "%i", d1");
+}
