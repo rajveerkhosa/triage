2ca0b1b90c2835600e3eb78e5567845f49c6f420
Skye Beebe <sbeebe@odin.cs.csubak.edu>
2024-10-08 09:13:05 -0700

    Adding my files

---
+++ sbeebe.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class Global {
+    public:
+
+}g;
+
+if (e->type == MotionNotify) {
+    //The mouse moved!
+   /* if (savex != e->xbutton.x || savey != e->xbutton.y) {
+        savex = e->xbutton.x;
+        savey = e->xbutton.y;
+        int y = g.yres - e->xbutton.y;*/
+        //Code placed here will execute whenever the mouse moves.
+        for (int i = 0; i < 2; i++) {
+            if (n < MAX_PARTICLES) {
+                particle[n].pos[0] = x;
+                particle[n].pos[1] = y;
+                particle[n].w = 4;
+                particle[n].h = 4;
+                particle[n].vel[0] = rnd() * 1.0 - 0.5;
+                particle[n].vel[1] = rnd() * 1.0 - 0.5;
+                ++n;
+            }
+
+images
