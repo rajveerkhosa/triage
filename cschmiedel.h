34fd1adb5acd69ce81d23bc25f327e1fee4ef59e
Christian Schmiedel <christians.schmiedel@gmail.com>
2024-10-18 09:28:32 -0700

    Create cschmiedel.h

---
+++ cschmiedel.h
@@ -0,0 +1,21 @@
+// cschmiedel.h
+
+#ifndef CSCHMIEDEL_H
+#define CSCHMIEDEL_H
+
+// Declare global health variables
+extern float currentHealth;
+extern const float maxHealth;
+extern bool gameOver;
+
+// Function prototypes for health bar
+void initializeHealthBar();
+void drawHealthBar(int xres, int yres);
+void dealDamage(float amount);
+void heal(float amount);
+void checkGameOver();
+
+// Function prototypes for rendering text
+void renderText(float x, float y, const char *text, float r, float g, float b);
+
+#endif // CSCHMIEDEL_H
